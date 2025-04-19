c9bc1092d74b5a8e3f80090bc81b1876b6f105be
gugolple <juanignacio.croce@gmail.com>
2025-04-19 13:09:43 +0200

    Test srcs
    Migrated from raw management to class based.

--- src/test_srcs/src/sim_main.cpp
+++ src/test_srcs/src/sim_main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <config.h>
-#include <format>
+#include <sstream>
 
 #include "Vhd44780.h"
 #include "verilated.h"
@@ -9,7 +9,9 @@ class test_hd44780 {
     public:
         test_hd44780(VerilatedContext& vc) : hd44780(Vhd44780{&vc}) {
             cycle = 0;
-            hd44780.rst = 0;
+            hcycle = 0;
+            le = 0;
+            hd44780.rst = 0; // Negated so is now acting as reset
             hd44780.clk = 0;
             hd44780.trg = 0;
             // Evaluate to enforce at least one cycle for validity of outputs
@@ -17,21 +19,60 @@ class test_hd44780 {
         }
 
         std::string to_string() {
-            return std::format("Cycle: {} Rst: {} Clk: Trg: {} Busy: {} E: {} RS: {} DB: {}\n",
-                cycle, (unsigned int)hd44780.rst, (unsigned int)hd44780.clk,
-                (unsigned int)hd44780.trg, (unsigned int)hd44780.busy,
-                (unsigned int)hd44780.e, (unsigned int)hd44780.rs, 
-                (unsigned int)hd44780.db
-            );
+            std::stringstream ss;
+            ss << "Cycle: " << cycle;
+            ss << " Rst: " << (unsigned int)hd44780.rst;
+            ss << " Clk: " << (unsigned int)hd44780.clk;
+            ss << " Trg: " << (unsigned int)hd44780.trg;
+            ss << " Busy: " << (unsigned int)hd44780.busy;
+            ss << " E: " << (unsigned int)hd44780.e;
+            ss << " RS: " << (unsigned int)hd44780.rs;
+            ss << " DB: " << (unsigned int)hd44780.db;
+            return ss.str();
+        }
+
+        void nextHalfCycle() {
+            syncVariables();
+            hd44780.clk = hd44780.clk ^ 0x01;
+            cycle += hd44780.clk; // Only on full waveform
+            hcycle++; // On each edge
+            hd44780.eval();
+        }
+
+        void nextHCycle(const unsigned int cycles) {
+            for(unsigned int i=0; i<cycles; i++) {
+                nextHalfCycle();
+            }
+        }
+
+        void nextCycle() {
+            nextHCycle(2);
+        }
+
+        bool changeEnable() {
+            return le ^ hd44780.e;
+        }
+
+        void setReset(unsigned char reset) {
+            hd44780.rst = reset;
+        }
+
+        unsigned char busy() {
+            return hd44780.busy;
+        }
+
+        unsigned long long getCycles() {
+            return cycle;
         }
 
     private:
         Vhd44780 hd44780;
         unsigned long long cycle;
+        unsigned long long hcycle;
+        unsigned char le;
 
-        void nextHalfCycle() {
-            hd44780.clk = hd44780.clk ^ 0x01;
-            hd44780.eval();
+        void syncVariables() {
+            this->le = hd44780.e;
         }
 };
 
@@ -43,36 +84,21 @@ int main(int argc, char **argv, char **env)
     unsigned long long int cnt = 0;
     unsigned long long int i = 0;
     const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
-    const std::unique_ptr<Vhd44780> top{new Vhd44780{contextp.get()}};
     test_hd44780 hd(*contextp);
     std::cout << hd.to_string() << std::endl;
-    contextp->internalsDump();  // See scopes to help debug
-    top->rst = 0;
-    top->clk = 0;
-    top->trg = 0;
-    unsigned char le = 0;
-    for(i=0; i<100; i++) {
-        if (top->e != le) {
-            std::cout << "ICycle: " << i;
-            std::cout << " Rst: " << (unsigned int)top->rst << " Clk: " << (unsigned int)top->clk << " Trg " << (unsigned int)top->trg;
-            std::cout << " Bsy: " << (unsigned int)top->busy << " E: " << (unsigned int)top->e << " RS: " << (unsigned int)top->rs << " DB: " << (unsigned int)top->db << std::endl;
+    for (int i=0; i<100; i++) {
+        hd.nextHalfCycle();
+        if (hd.changeEnable()) {
+            std::cout << hd.to_string() << std::endl;
         }
-        le = top->e;
-        top->eval();
-        top->clk = top->clk ^ 0x01;
     }
-    top->rst = 1;
-    while(top->busy) {
-        if (top->e != le) {
-            std::cout << "SCycle: " << i;
-            std::cout << " Rst: " << (unsigned int)top->rst << " Clk: " << (unsigned int)top->clk << " Trg " << (unsigned int)top->trg;
-            std::cout << " Bsy: " << (unsigned int)top->busy << " E: " << (unsigned int)top->e << " RS: " << (unsigned int)top->rs << " DB: " << (unsigned int)top->db << std::endl;
+    hd.setReset(1);
+    while(hd.busy()) {
+        hd.nextHalfCycle();
+        if (hd.changeEnable()) {
+            std::cout << hd.to_string() << std::endl;
         }
-        le = top->e;
-        top->eval();
-        top->clk = top->clk ^ 0x01;
-        i++;
     }
-    std::cout << "Cycle count: " << cnt << std::endl;
+    std::cout << "Cycle count: " << hd.getCycles() << std::endl;
     exit(0);
 }
