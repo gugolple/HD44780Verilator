d02a8073ebd6d0d755d7c1f106d779ffd8e55acf
gugolple <juanignacio.croce@gmail.com>
2025-07-11 12:07:09 +0000

    TestSrcs
    Added desired functionality.
    Now the displays will be logged as part of the test suite for the
    apropriate context.

--- src/test_srcs/verilator_overrides/custom_overrides.cpp
+++ src/test_srcs/verilator_overrides/custom_overrides.cpp
@@ -21,27 +21,28 @@
 // https://stackoverflow.com/questions/55424746/is-there-an-analogous-function-to-vsnprintf-that-works-with-stdstring
 // https://stackoverflow.com/questions/56331128/call-to-snprintf-and-vsnprintf-inside-a-variadic-function
 int my_printf( const char * format, ... ) {
+#define BUFSIZE 1024
+    char szBuff[BUFSIZE] = "\0";
+
     // Buffer
-    std::string result;
     va_list args, args_copy;
 
     // Open variadic macros
     va_start(args, format);
     va_copy(args_copy, args);
 
-    size_t size = vsnprintf(nullptr, 0, format, args) + 1; 
+    const size_t size = vsnprintf(nullptr, 0, format, args_copy) + 1; 
+    //printf("%ld\n", size);
     assert(size>0); // Sanity check, force only positives
+    assert(size<BUFSIZE); // Sanity check, force only positives
     va_end(args_copy);
 
     // Format the string
-    char *szBuff = new char[size];
     vsnprintf(szBuff, size, format, args);
     va_end(args);
 
+    //printf("%s", szBuff);
 
-    std::cout << szBuff << std::endl;
-
-    // Enjoy the result
-    //UNSCOPED_INFO(szBuff);
-    delete[] szBuff;
+    UNSCOPED_INFO(szBuff);
+    return size;
 }
